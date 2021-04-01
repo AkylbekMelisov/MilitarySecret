d4169bf1391f2464e68ebc59d480e58ebaa3d714
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-04-01 18:32:11 +0600

    all migrations and settings

---
+++ military_secret/accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rest_framework import views, viewsets, generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializers import *
+
+
+class RegisterView(views.APIView):
+
+    def get(self, request, *args, **kwargs):
+        users = User.objects.all()
+        serializer = RegisterSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors)
+
+
+class CarView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            cars = Car.objects.all()
+        else:
+            cars = Car.objects.filter(dossier=request.user.dossier)
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
---
+++ military_secret/documents/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsSueruserOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(request.method in SAFE_METHODS or request.user and request.user.is_superuser)
