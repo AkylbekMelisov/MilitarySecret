a986ccb126e64577d7c21123770cdc308213076b
AkylbekMelisov <akylbek.melisov@gmail.com>
2021-05-07 14:15:56 +0600

    add name = crate

--- military_secret/accounts/urls.py
+++ military_secret/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', RegisterView.as_view()),
+    path('', RegisterView.as_view(), name='create'),
     path('cars/', CarView.as_view())
 ]
