2f450d682fc99f986bbf7c0b10d13dcb48c7efcc
AkylbekMelisov <akylbek.melisov@gmail.com>
2021-05-07 14:16:53 +0600

    add status for request

--- military_secret/accounts/views.py
+++ military_secret/accounts/views.py
@@ -18,7 +18,7 @@ class RegisterView(views.APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
 class CarView(views.APIView):
